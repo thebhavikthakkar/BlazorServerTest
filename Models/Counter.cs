073c98b857c5a644d7ad002c9ea452f4610d8d4b
thebhavikthakkar <54937805+thebhavikthakkar@users.noreply.github.com>
2023-03-30 11:29:53 +0530

    Add files via upload

---
+++ Models/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace BlazorServer.Models
+{
+    public class Counter
+    {
+        public int Counter1 { get; set; }
+
+        public int Counter2 { get; set; }
+
+        public int Counter3 { get; set; }
+
+        public int Counter4 { get; set; }
+
+        public StringBuilder Log { get; set; } = new();
+    }
+}
---
+++ Services/CounterProcessor.cs
@@ -0,0 +1,46 @@
+﻿using BlazorServer.Models;
+
+namespace BlazorServer.Services;
+
+public class CounterProcessor
+{
+    public event Action? OnChange;
+
+    public Counter counter = new Counter();
+
+    public void UpdateCounter(int currentCounter)
+    {
+        switch (currentCounter)
+        {
+            case 1:
+                counter.Counter1 = counter.Counter1 + 1;
+                Log(1, counter.Counter1);
+                break;
+            case 2:
+                counter.Counter2 = counter.Counter1 + 1;
+                Log(2, counter.Counter2);
+                break;
+            case 3:
+                counter.Counter3 = counter.Counter2 + 1;
+                Log(3, counter.Counter3);
+                break;
+            case 4:
+                counter.Counter4 = counter.Counter3 + 1;
+                Log(4, counter.Counter4);
+                break;
+        }
+
+        NotifyStateChanged();
+    }
+
+    private void Log(int currentCounter, int value)
+    {
+        counter.Log.Append(Environment.NewLine);
+        counter.Log.Append($"Dynamic Component {currentCounter} is starting – Counter {value - 1}");
+        counter.Log.Append(Environment.NewLine);
+        counter.Log.Append($"Dynamic Component {currentCounter} is finishing – Counter {value}");
+        counter.Log.Append(Environment.NewLine);
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
